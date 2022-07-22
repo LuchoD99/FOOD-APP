1ab0e97f95084d9d66dd2ca417a4df9263b7fdf6
LuchoD99 <luciano.diazocampo@gmail.com>
2022-07-21 23:09:03 -0300

    modified

--- api/src/models/TypeOfDiet.js
+++ api/src/models/TypeOfDiet.js
@@ -1,15 +1,13 @@
 const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
-    // defino el modelo
     sequelize.define('diet', {
-        // id: {
-        //     type: DataTypes.INTEGER,
-        //     allowNull: false,
-        //     primaryKey: true,
-        //     unique: true,
-        //     autoIncrement: true,
-        // },
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
         name: {
             type: DataTypes.STRING,
             allowNull: false,
